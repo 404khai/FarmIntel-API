609ce96530a27d84c78ef64945a16ced382982fe
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-11-16 12:34:24 +0100

    feat(noti): add notifications views, urls

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls), 
     path('users/', include('users.urls')),
     path("orgs/", include("organizations.urls")),
-
+    path("notifications/", include("notifications.urls")),
 ]
